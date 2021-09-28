b622da53b2f0b79d6dd43d2802f856619c3cc85f
ChristianeB <90196976+Sup3rwoman@users.noreply.github.com>
2021-09-28 13:59:36 +0200

    Implemented sorting algorithm

--- Thanos.java
+++ Thanos.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Thanos {
 
@@ -12,21 +11,38 @@ public class Thanos {
         heroes.add(new Hero("Thor", 1501));
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
-
+        Collections.sort(heroes);
 
         System.out.println("\nOrder by name:");
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
+        Comparator<Hero> heroAgeComparator = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero o1, Hero o2) {
+                return o2.getAge() - o1.getAge();
+                //Long Version:
+                /*if(o2.getAge() < o1.getAge()) {
+                    return -1;
+                } else if (o2.getAge() > o1.getAge()) {
+                    return 1;
+                } else {
+                    return 0;
+                }*/
+            }
+        };
 
+        Collections.sort(heroes,heroAgeComparator);
 
+        //Sorting with Lambda Expression:
+        //Collections.sort(heroes, Comparator.comparing(Hero::getAge).reversed());
         System.out.println("\nOrder by age:");
         showList(heroes);
     }
 
     private static void showList(List<Hero> heroes) {
-        for (Hero heroe : heroes) {
-            System.out.println(heroe.getName() + ", " + heroe.getAge());
+        for (Hero hero : heroes) {
+            System.out.println(hero.getName() + ", " + hero.getAge());
         }
     }
 }
